d5504b678c19862159dcbea76f946f68db9f6e8c
Suhaib Malik <suhaibmalik6397@gmail.com>
2023-05-08 17:45:32 +0530

    Initial Commit

---
+++ Backend/shopziel/src/main/java/com/shopziel/ShopzielApplication.java
@@ -0,0 +1,13 @@
+package com.shopziel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopzielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopzielApplication.class, args);
+	}
+
+}
---
+++ Backend/shopziel/src/test/java/com/shopziel/shopziel/ShopzielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopziel.shopziel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopzielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
