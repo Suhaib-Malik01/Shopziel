ee1b788635adb6dc4779422351f021984a3836f2
Suhaib Malik <suhaibmalik6397@gmail.com>
2023-05-09 13:58:30 +0530

    implemented jwt

--- Backend/shopziel/src/main/java/com/shopziel/controller/CustomerController.java
+++ Backend/shopziel/src/main/java/com/shopziel/controller/CustomerController.java
@@ -1,5 +1,19 @@
 package com.shopziel.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/customers")
+@CrossOrigin(origins = "*")
 public class CustomerController {
+
+    @PostMapping("/")
+    public String hello(){
+        return "hello world";
+    }
     
 }
