c5d2ccbe910a0f267873880cb21ff36efb80e53c
Akash Samanta <101733074+aks-samanta@users.noreply.github.com>
2023-05-11 13:20:27 +0530

    login error not resolved

--- Backend/shopziel/src/main/java/com/shopziel/service/MyUserDetailsService.java
+++ Backend/shopziel/src/main/java/com/shopziel/service/MyUserDetailsService.java
@@ -14,75 +14,34 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.shopziel.models.Admin;
+import com.shopziel.models.AppUser;
 import com.shopziel.models.Customer;
 import com.shopziel.models.Seller;
 import com.shopziel.repository.AdminRepository;
+import com.shopziel.repository.AppUserRepository;
 import com.shopziel.repository.CustomerRepository;
 import com.shopziel.repository.SellerRepository;
-
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
 	@Autowired
-	private CustomerRepository customerRepository;
-
-	@Autowired
-	private AdminRepository adminRepository;
-
-	@Autowired
-	private SellerRepository sellerRepository;
+	private AppUserRepository appUserRepository;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-		Optional<Customer> existingCustomer = customerRepository.findByEmail(username);
+		Optional<AppUser> existingappUser = appUserRepository.findByEmail(username);
 
+		if (existingappUser.isPresent()) {
 
-		if (existingCustomer.isPresent()) {
-
-			Customer customer = existingCustomer.get();
+			AppUser appUser = existingappUser.get();
 			List<GrantedAuthority> authorities = new ArrayList<>();
 
-			SimpleGrantedAuthority sga = new SimpleGrantedAuthority("ROLE_CUSTOMER");
+			SimpleGrantedAuthority sga = new SimpleGrantedAuthority(appUser.getRole());
 			authorities.add(sga);
 
-			User user = new User(customer.getEmail(), customer.getPassword(), authorities);
-
-
-			return user;
-
-		}
-
-		Optional<Seller> existingSeller = sellerRepository.findByEmail(username);
-
-		if (existingSeller.isPresent()) {
-
-			Seller seller = existingSeller.get();
-			List<GrantedAuthority> authorities = new ArrayList<>();
-
-			SimpleGrantedAuthority sga = new SimpleGrantedAuthority("ROLE_SELLER");
-
-			authorities.add(sga);
-			User user = new User(seller.getEmail(), seller.getPassword(), authorities);
-			
-
-			return user;
-
-		}
-
-		Optional<Admin> existingAdmin = adminRepository.findByEmail(username);
-
-		if (existingAdmin.isPresent()) {
-
-			Admin admin = existingAdmin.get();
-
-			List<GrantedAuthority> authorities = new ArrayList<>();
-			SimpleGrantedAuthority sga = new SimpleGrantedAuthority("ROLE_ADMIN");
-			authorities.add(sga);
-
-			User user = new User(admin.getEmail(), admin.getPassword(), authorities);
-			
+			User user = new User(appUser.getEmail(), appUser.getPassword(), authorities);
 
 			return user;
 
@@ -93,4 +52,3 @@ public class MyUserDetailsService implements UserDetailsService {
 	}
 
 }
-
