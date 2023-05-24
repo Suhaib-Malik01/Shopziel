c355d34a571dacf86bc8bc0ddb5dbee72433f6ab
Suhaib Malik <suhaibmalik6397@gmail.com>
2023-05-24 15:27:09 +0530

    updated application.properties

--- Backend/shopziel/src/main/java/com/shopziel/dto/ProductDto.java
+++ Backend/shopziel/src/main/java/com/shopziel/dto/ProductDto.java
@@ -4,17 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-<<<<<<< HEAD
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
-import com.shopziel.models.Category;
-=======
->>>>>>> 74b3de8c08c80eefcec19196e0ffafb7e4931e3b
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Data
 public class ProductDto {
