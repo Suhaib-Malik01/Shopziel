96f47c5a5aa1f052bac94f4463e4f3473500eddb
Suhaib Malik <suhaibmalik6397@gmail.com>
2023-05-26 23:48:01 +0530

    fixed cors origin

--- Backend/shopziel/src/main/java/com/shopziel/config/AppConfig.java
+++ Backend/shopziel/src/main/java/com/shopziel/config/AppConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
@@ -30,6 +31,7 @@ public class AppConfig {
                 .requestMatchers(AUTH_WHITE_LIST).permitAll()
                 .requestMatchers("/api/products/*").permitAll() // permit only get requests
                 .anyRequest().authenticated().and()
+                .addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class)
                 .addFilterAfter(new JwtTokenGeneratorFilter(), BasicAuthenticationFilter.class)
                 .addFilterBefore(new JwtTokenValidatorFilter(), BasicAuthenticationFilter.class).formLogin().and()
                 .httpBasic();
