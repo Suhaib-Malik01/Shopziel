9ee233aa268ded1aea979c56a22c27ed4623f3b9
Suhaib Malik <suhaibmalik6397@gmail.com>
2023-05-29 21:52:25 +0530

    update getCategory menthod

--- Backend/shopziel/src/main/java/com/shopziel/service/CategoryServiceImpl.java
+++ Backend/shopziel/src/main/java/com/shopziel/service/CategoryServiceImpl.java
@@ -8,8 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.shopziel.dto.CategoryDto;
-
+import com.shopziel.dto.ProductDto;
 import com.shopziel.models.Category;
+import com.shopziel.models.Product;
 import com.shopziel.repository.CategoryRepository;
 import com.shopziel.exception.CategoryException;;
 
@@ -24,7 +25,7 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public CategoryDto addCategory(CategoryDto categoryDto) {
-        
+
         Category category = categoryRepository.save(modelMapper.map(categoryDto, Category.class));
 
         return modelMapper.map(category, CategoryDto.class);
@@ -46,12 +47,9 @@ public class CategoryServiceImpl implements CategoryService {
         Category category = categoryRepository.findById(categoryId)
                 .orElseThrow(() -> new CategoryException("Category not found"));
 
-        return modelMapper.map(category,CategoryDto.class);
+        CategoryDto categoryDto = modelMapper.map(category, CategoryDto.class);
+
+        return categoryDto;
     }
-
-    
-   
-
-    
 
 }
